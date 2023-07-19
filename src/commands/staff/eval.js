d4f41042ed27e1d896c7a8499a10a885d5f98ba1
Jacob Bond <jacobpbond4@gmail.com>
2023-07-19 22:14:48 +0100

    feat: eval

---
+++ src/commands/staff/eval.js
@@ -0,0 +1,29 @@
+const { ApplicationCommandOptionType } = require('../../constants/Types');
+const Command = require('../../framework/Command');
+const { inspect } = require('util');
+
+module.exports = class extends Command {
+
+  constructor (...args) {
+    super(...args, {
+      name: 'eval',
+      description: 'Evaluate.',
+      options: [
+        { type: ApplicationCommandOptionType.String, required: true, name: 'string', description: 'eval js code' }
+      ]
+    });
+  }
+
+  async run (ctx) {
+    let resp;
+    try {
+      resp = await eval(`(async function(){${ctx.args.string.value}}).call()`);
+    } catch (e) {
+      resp = e.stack;
+    }
+
+    return new Command.InteractionResponse()
+      .setContent(`\`\`\`js\n${inspect(resp, 2).substring(0, 1900)}`);
+  }
+
+};
