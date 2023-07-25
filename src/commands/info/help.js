1127f1963efa5950288ab3898b9404fcba666197
Jacob Bond <jacobpbond4@gmail.com>
2023-07-25 20:46:50 +0100

    feat: help command improvement

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -11,17 +11,17 @@ module.exports = class extends Command {
 
   async run ({ user }) {
     return new Command.InteractionEmbedResponse()
-      .setTitle('Rubix')
       .setColour('blue')
-      .setDescription([
+      .setDescription(['### Rubix',
         `Hi **${user.globalName}**! Thank you for using Rubix, a multi-purpose Discord bot that packs a punch.`,
         'To view a list of my commands, type `/` and click on my avatar.',
-        '\nfeature 1',
-        'feature 2',
-        'feature 3',
+        '\n- Simplified implementation of Discord AutoMod.',
+        '- Reaction roles made easy.',
+        '- Custom commands for your server.',
         '\nWant me in your server? Click the invite button below!'
       ].join('\n'))
-      .addButton({ label: 'Invite Me', url: 'https://discord.com/api/oauth2/authorize?client_id=1125509497079726182&scope=bot%20applications.commands' });
+      .addButton({ label: 'Invite Me', url: 'https://discord.com/api/oauth2/authorize?client_id=1125509497079726182&scope=bot%20applications.commands' })
+      .addButton({ label: 'Support Server', url: 'https://discord.gg/pKtCuVv' });
   }
 
 };
