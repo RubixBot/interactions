a20a117dda12af149acef64c1732055d331c5701
itsjakeb <jacobpbond4@gmail.com>
2023-08-17 01:06:23 +0100

    feat: invite command and extra help

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -15,12 +15,14 @@ module.exports = class extends Command {
       .setDescription(['### Rubix',
         `Hi **${user.globalName}**! Thank you for using Rubix, a multi-purpose Discord bot that packs a punch.`,
         'To view a list of my commands, type `/` and click on my avatar.',
+        'Some more complicated commands may have a `help` subcommand to help guide you with that particular command.',
         '\n- Simplified implementation of Discord AutoMod.',
         '- Reaction roles made easy.',
         '- Custom commands for your server.',
+        '- Games to play with friends!',
         '\nWant me in your server? Click the invite button below!'
       ].join('\n'))
-      .addButton({ label: 'Invite Me', url: 'https://discord.com/api/oauth2/authorize?client_id=1125509497079726182&scope=bot%20applications.commands' })
+      .addButton({ label: 'Invite Me', url: `https://discord.com/api/oauth2/authorize?client_id=${this.core.config.applicationID}&scope=bot%20applications.commands` })
       .addButton({ label: 'Support Server', url: 'https://discord.gg/pKtCuVv' });
   }
 
